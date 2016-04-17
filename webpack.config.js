01cb96db285f69535030eeff3fa5c75843d71109
jerryshew <jerryshew@gmail.com>
2016-04-17 22:49:09 +0800

    env

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 // var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var publicPath = process.env.NODE_ENV === 'dev' ? '/dist/' : '';
 
 module.exports = {
-
-    entry: "./src/app.js",
+    entry: "./entre.js",
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'app.js',
-        publicPath: '/dist/'
+        filename: 'dot.js',
+        publicPath: publicPath
     },
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
@@ -29,10 +29,20 @@ module.exports = {
             }, { 
                 test: /\.css$/,
                 loader: "style!css" // ExtractTextPlugin.extract("style-loader", "css-loader") 
+            }, { 
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=8192&name=./image/[name].[ext]' 
             },  { 
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: "babel" 
+                loader: "babel",
+            }, {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+            }, {
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                loader : 'url-loader?limit=10000&name=./font/[name].[ext]'
             }
         ]
     },
